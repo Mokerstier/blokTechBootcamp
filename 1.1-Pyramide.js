941007647256baa7a351d29497b478bf0633caea
Wouter <33430655+Mokerstier@users.noreply.github.com>
2019-04-24 17:23:42 +0200

    Delete 1.1-Pyramide.js

--- 1.1-Pyramide.js
+++
@@ -1,5 +0,0 @@
-// #1 PIRAMIDE
-
-for (let hekkie = "#"; hekkie.length < 8; hekkie += "#") {  //loop door de string en voeg "#" toe 
-     console.log(hekkie)                                    //na elke loop tot de string hekkie 7 tekens bevat
-}    
