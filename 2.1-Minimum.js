b5ed979da989eedbde1a41994cf5c079f40b911d
Wouter <33430655+Mokerstier@users.noreply.github.com>
2019-04-25 10:06:40 +0200

    Delete 2.1-Minimum.js

--- 2.1-Minimum.js
+++
@@ -1,6 +0,0 @@
-function min (x, y){ 
-    if (x === y){                   // zijn x en y evengroot bijv:(2, 2) 
-        return "both are the same"; //print "both are the same"
-    } else if (x > y){              // is x groter als y 
-        return y;                   // print y
-    } else return x;}               // anders print x (want die is dan kleiner)
